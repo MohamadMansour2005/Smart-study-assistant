9bdd39f3555d084b338b18e5665640d4c815b1b4
MohamadMansour2005 <mim33@mail.aub.edu>
2026-04-16 04:34:55 -0700

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,1 +1,16 @@
 
+module "lambda" {
+  source = "./modules/lambda"
+
+  project_name = var.project_name
+  region       = var.region  
+  
+}
+
+module "api_gateway" {
+  source = "./modules/api_gateway"
+
+  project_name       = var.project_name
+  region             = var.region
+  lambda_invoke_arn  = module.lambda.lambda_invoke_arn
+}

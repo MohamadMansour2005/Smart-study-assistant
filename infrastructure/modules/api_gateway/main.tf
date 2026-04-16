627bbf0a74fe90e6f0733d80be2c2acc5c4a000a
MohamadMansour2005 <mim33@mail.aub.edu>
2026-04-16 04:33:19 -0700

    Update main.tf

--- infrastructure/modules/api_gateway/main.tf
+++ infrastructure/modules/api_gateway/main.tf
@@ -1,1 +1,46 @@
 
+resource "aws_api_gateway_rest_api" "api" {
+  name = "${var.project_name}-api"
+}
+
+resource "aws_api_gateway_resource" "test" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "test"
+}
+
+resource "aws_api_gateway_method" "test_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.test.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.test.id
+  http_method = aws_api_gateway_method.test_get.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+
+  uri = var.lambda_invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deploy" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_stage" "dev" {
+  stage_name    = "dev"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.deploy.id
+}
+
+output "api_url" {
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/dev/test"
+}
